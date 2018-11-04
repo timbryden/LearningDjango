06066d5f8cf356f0f189847d0e3e09e74e8d4287
tim bryden <tbryden@calumo.com>
2018-11-04 18:31:43 +1100

    Add variables to Index.html and update views

--- BasicProject/BasicProject/settings.py
+++ BasicProject/BasicProject/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'app',
     # Add your apps here to enable them
+    'HelloDjangoApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from datetime import datetime
+
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Brydos!",
+            'content': " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
