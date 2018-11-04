7deeface5c091e264d14c0f540dc502d3e0495dd
tim bryden <tbryden@calumo.com>
2018-11-04 19:37:44 +1100

    Adding a page

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -18,6 +18,7 @@ import app.views
 urlpatterns = [
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^about$', HelloDjangoApp.views.about, name='about'),
 
 
     # Examples:
