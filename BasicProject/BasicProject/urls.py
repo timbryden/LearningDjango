59cd2a69ee34a9200fb6862d4523c3e03858c190
tim bryden <tbryden@calumo.com>
2018-11-04 16:52:22 +1100

    Add HelloDjangoApp

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -3,8 +3,9 @@ Definition of urls for BasicProject.
 """
 
 from datetime import datetime
-from django.conf.urls import url
+from django.conf.urls import include, url
 import django.contrib.auth.views
+import HelloDjangoApp.views
 
 import app.forms
 import app.views
@@ -15,28 +16,32 @@ import app.views
 # admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+
+
     # Examples:
-    url(r'^$', app.views.home, name='home'),
-    url(r'^contact$', app.views.contact, name='contact'),
-    url(r'^about', app.views.about, name='about'),
-    url(r'^login/$',
-        django.contrib.auth.views.login,
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': app.forms.BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title': 'Log in',
-                'year': datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        django.contrib.auth.views.logout,
-        {
-            'next_page': '/',
-        },
-        name='logout'),
+    #url(r'^$', app.views.home, name='home'),
+    #url(r'^contact$', app.views.contact, name='contact'),
+    #url(r'^about', app.views.about, name='about'),
+    #url(r'^login/$',
+    #    django.contrib.auth.views.login,
+    #    {
+    #        'template_name': 'app/login.html',
+    #        'authentication_form': app.forms.BootstrapAuthenticationForm,
+    #        'extra_context':
+    #        {
+    #            'title': 'Log in',
+    #            'year': datetime.now().year,
+    #        }
+    #    },
+    #    name='login'),
+    #url(r'^logout$',
+    #    django.contrib.auth.views.logout,
+    #    {
+    #        'next_page': '/',
+    #    },
+    #    name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
